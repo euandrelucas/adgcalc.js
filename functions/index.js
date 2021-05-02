f390816e70569b076ac86296f91eeb83d60f6bb3
yxqsnz <bolaknt@gmail.com>
2021-05-02 16:09:39 -0300

    Adicionado verificação de número por causa do Eleven.

--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,9 @@
 // exemplo: calcular('+', 1, 2, 3, 4)
 function calcular(operador,...ns) {
+    if(ns.some(isNaN)) throw TypeError('unknown number.');
     return eval(`${ns.join(operador)}`);
 }
+
 // calc.somar(1, 1, 1)
 module.exports = {
     somar: (...numeros) => calcular('+', ...numeros),
